8f1bb9f447662f9ff4551cb2f4911dc5519d4c97
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-07-05 21:20:14 -0700

    Create tables in database

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,15 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE players ( id SERIAL,
+                       name TEXT );
 
+CREATE TABLE matches ( matchid SERIAL,
+                       round INTEGER,
+                       player1 INTEGER,
+                       player2 INTEGER,
+                       winner INTEGER );
+
+CREATE TABLE scoreboard ( id INTEGER,
+                          wins INTEGER,
+                          MATCHES INTEGER );
