4e94716d26273e316ce345d967c5fc1e962a84c7
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-07-05 22:13:52 -0700

    Add register player function

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -47,6 +47,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    DB = connect()
+    c = DB.cursor()
+    c.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+    DB.commit()
+    DB.close()
 
 
 def playerStandings():
